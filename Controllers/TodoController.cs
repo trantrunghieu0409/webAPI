2e57720c58e199ad0e229043bfac9a47f5905146
lonelynight-dark <52921466+trantrunghieu0409@users.noreply.github.com>
2022-05-17 17:26:22 +0700

    init

---
+++ Controllers/TodoController.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+
+namespace WebAPI.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TodoController : ControllerBase
+	{
+		private readonly TodoContext _todoContext;
+		public TodoController(TodoContext todoContext)
+		{
+			_todoContext = todoContext;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAction()
+		{
+			var res = _todoContext.TodoItem.FromSqlRaw("SELECT id, title, isComplete FROM FUNC_TODOINFO() Order By Title DESC")
+				.ToList();
+			return Ok(res);
+		}
+
+		[HttpPost("{comment}")]
+		//[Route()]
+		public async Task<IActionResult> GetActionWithComment(TodoItem todoItem, string comment = "Default")
+		{
+			_todoContext.TodoItem.FromSqlRaw($"EXEC DBO.PROC_ADDTODO @TITLE='{todoItem.Title}', @COMMENT='{comment}'");
+			return Ok("OK");
+		}
+
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetActionByID(int id)
+		{
+			var todoItem = await _todoContext.TodoItem.FindAsync(id);
+			if (todoItem == null) { return NotFound(); }
+			return Ok(todoItem);
+		}
+
+
+		[HttpGet]
+		[Route("cake-by-id")]
+		public async Task<IActionResult> GetActionByID_Route(int id)
+		{
+			var todoItem = await _todoContext.TodoItem.FindAsync(id);
+			if (todoItem == null) { return NotFound(); }
+			return Ok(todoItem);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> PostAction(TodoItem todoItem)
+		{
+			_todoContext.TodoItem.Add(todoItem);
+			await _todoContext.SaveChangesAsync();
+			return Created("/", todoItem);
+		}
+
+		[HttpPut]
+		public async Task<IActionResult> PutAction(TodoItem todoItem)
+		{
+			_todoContext.Update(todoItem);
+			await _todoContext.SaveChangesAsync();
+			return Ok(todoItem);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> DeleteAction(int id)
+		{
+			var todo = await _todoContext.TodoItem.FindAsync(id);
+			if (todo == null)
+			{
+				return NotFound();
+			}
+			_todoContext.Remove(todo);
+			await _todoContext.SaveChangesAsync();
+			return Created("/", todo);
+		}
+	}
+}
---
+++ Models/TodoItem.cs
@@ -0,0 +1,9 @@
+﻿namespace WebAPI.Models
+{
+	public class TodoItem
+	{
+		public int Id { get; set; }	
+		public string? Title { get; set; }
+		public Int16 IsComplete { get; set; }
+	}
+}
